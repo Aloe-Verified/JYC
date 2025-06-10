7d7d18ddcebcb7459371a037fc93fe87acfc7b66
Aidan <aidankw12345@gmail.com>
2025-06-10 17:37:31 -0400

    merge backend folder from auth-setup branch

---
+++ backend/src/main/java/com/JYC/note_taking_assistant/NoteTakingAssistantApplication.java
@@ -0,0 +1,15 @@
+package com.JYC.note_taking_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class NoteTakingAssistantApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NoteTakingAssistantApplication.class, args);
+	}
+
+}
